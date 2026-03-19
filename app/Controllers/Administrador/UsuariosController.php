7e12e019efc0cbedd152038ec1e612a67cb4f022
Diego Tasayco Huaccamayta <tasaycodiego76@gmail.com>
2026-03-19 00:11:41 -0500

    Agregando vista de usuarios con tabla + buúsqueda (rutas/controlador/modelo/vista creados); botones de editar/deshabilitar estaán, pero la loógica de acciones auún no estaá implementada

---
+++ app/Controllers/Administrador/UsuariosController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers\Administrador;
+
+use App\Controllers\BaseController;
+use App\Models\UsuarioModel;
+use App\Models\EmpresaModel;
+
+class UsuariosController extends BaseController
+{
+    public function index()
+{
+    $usuarioModel  = new UsuarioModel();
+    $empresaModel  = new EmpresaModel();
+
+    $empresas = $empresaModel->obtenerConStats();
+
+    return view('administrador/usuarios/lista', [
+        'paginaActual'   => 'usuarios',
+        'titulo'         => 'USUARIOS',
+        'usuarios'       => $usuarioModel->obtenerTodosConArea(),
+        'empresas'       => $empresas,
+        'contador_total' => count($empresas),
+    ]);
+    }
+}
